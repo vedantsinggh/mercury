a40b9920455478182702413d4f2583a2a756d4cd
KartikayTayal <ktayal1999@gmail.com>
2026-04-17 22:26:02 +0530

    Made changes to logger

--- src/logger/setup.py
+++ src/logger/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+import os
 
 package_name = 'logger'
 
@@ -6,6 +7,11 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     entry_points={
