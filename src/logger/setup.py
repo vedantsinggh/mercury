11949bad8083e92e66fecc9a7a2611e497f355a5
ugv <ugv@dtu.ac.in>
2026-04-18 17:04:57 +0530

    Revert "."
    This reverts commit a40b9920455478182702413d4f2583a2a756d4cd.

--- src/logger/setup.py
+++ src/logger/setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup
-import os
 
 package_name = 'logger'
 
@@ -7,11 +6,6 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
     install_requires=['setuptools'],
     zip_safe=True,
     entry_points={
